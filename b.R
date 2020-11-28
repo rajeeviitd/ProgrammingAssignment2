24b2aaa68a17aa9eb76ef256e6f4f7fdddf4f3e3
Sunidhi Sharma <ssharmassunidhi@gmail.com>
2020-11-28 19:31:57 +0530

    LEXICAL SCOPING CACHE MATRIX

--- b.R
+++ b.R
@@ -1,12 +1,29 @@
+## A pair of functions that cache the inverse of matrix
+
+
+
+## The following creates a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x=matrix()){
+  
+  ## The following initialises the inverse property
   inv <- NULL
+  
+  ## The following method is used to set matrix
   set <- function(y){
     x <<- y
     inv <<- NULL
   }
+  
+  ## The following method is used to get the matrix
   get <- function() {x}
+  
+  ## The following method is used to set the inverse of the matrix
   setInverse <- function(inverse) {inv <<- inverse}
+  
+  ## The following method is used to get the inverse of the matrix
   getInverse <- function() {inv}
+  
+  ## The following method is used to return a list of the methods
   list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 #Write a short comment describing this function
@@ -17,8 +34,16 @@ cacheSolve <- function(x, ...) {
     message("getting cached data")
     return(inv)
   }
+  
+  ## The following method is used to get the matrix from our object 
   mat <- x$get()
+  
+  ## the following method is used to solve the inverse using matrix multiplication
   inv <- solve(mat, ...)
+  
+  ## The following method is used to set the inverse to the object
   x$setInverse(inv)
+  
+  ## the following method is used to return the matrix
   inv
 }
